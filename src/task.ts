8e62d0b3eb996da7a07ed2353fe44f3b78f9c89c
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-21 22:24:53 +0530

    feat: add Task class module

---
+++ src/task.ts
@@ -0,0 +1,38 @@
+type Priorities = 0 | 1 | 2
+
+type TaskObject = {
+  title: string
+  description: string
+  dueDate: string
+  priority: Priorities
+  completed?: boolean
+}
+
+export default class Task {
+  title: string
+  description: string
+  dueDate: string
+  _priority: Priorities
+  completed: boolean
+
+  constructor({ title, description, dueDate, priority, completed = false }: TaskObject) {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this._priority = priority
+    this.completed = completed
+  }
+  set priority(value: Priorities) {
+    if ([0, 1, 2].includes(value)) {
+      this._priority = value
+    }
+    return
+  }
+  get priority() {
+    return this._priority
+  }
+
+  toggleCompletion() {
+    this.completed = !this.completed
+  }
+}
