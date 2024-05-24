267c9a961fe3e57fe0704118395afd63d8ad3c79
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-24 09:40:35 +0530

    refactor: Update storage module for list handling
    - Update `storeItem` to `storeList` for creating
    and storing lists by index.
    - Remove `removeItem` function.
    - Simplify the `storeList` function to include
    list creation logic.

--- src/storage.ts
+++ src/storage.ts
@@ -1,22 +1,23 @@
 import Task from "./task"
-export function storeItem(list: { tasks: {}[] }, name: string) {
-  localStorage.setItem(`${name}`, JSON.stringify(list))
+import { createList } from "./list"
+
+export function storeList(name: string, index: number) {
+  const newList = createList(name)
+  localStorage.setItem(`${index}`, JSON.stringify(newList))
 }
+
 export function clearStorage() {
   localStorage.clear()
 }
-export function retrieveItem(listIndex: number) {
-  const itemToParse = localStorage.getItem(`${listIndex}`)
+
+export function retrieveItem(index: number) {
+  const itemToParse = localStorage.getItem(`${index}`)
   if (itemToParse) {
     const parsed = JSON.parse(itemToParse)
     parsed.tasks.forEach((task: Task) => {
       Object.setPrototypeOf(task, Task.prototype)
     })
-
     return parsed
   }
   return
-}
-export function removeItem(listIndex: number) {
-  localStorage.removeItem(`${listIndex}`)
 }
