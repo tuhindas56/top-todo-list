020b5992c46bd86769b6fa66051780caec54062f
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-24 09:43:28 +0530

    feat: add DOM utility functions
    - Add `createListElement` function to create
    list elements with index.
    - Add `createButtonElement` function to create
    button elements with text.
    - Add `assignClasses` function to apply multiple
     classes to an element.
    - Add `appendItem` function to append child
    elements to a parent element.

--- src/domUtils.ts
+++ src/domUtils.ts
@@ -5,6 +5,25 @@ export function openDialog(id: string) {
 
 export function closeDialog(id: string) {
   const dialog = document.querySelector(`dialog#${id}_form`) as HTMLDialogElement
-
   dialog.close()
+}
+
+export function createListElement(index: number) {
+  const newListElement = document.createElement("li") as HTMLLIElement
+  newListElement.dataset.index = `${index}`
+  return newListElement
+}
+
+export function createButtonElement(name: string) {
+  const newButtonElement = document.createElement("button") as HTMLButtonElement
+  newButtonElement.textContent = name
+  return newButtonElement
+}
+
+export function assignClasses(element: HTMLElement, ...classes: string[]) {
+  classes.forEach((item) => element.classList.add(item))
+}
+
+export function appendItem(parent: HTMLElement, child: HTMLElement) {
+  parent.append(child)
 }
