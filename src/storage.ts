f18f2b97efd4c9e4a89cf65c3e6f1f81891c3282
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-25 21:08:47 +0530

    refactor: Revert storage logic
    - Revert list storing logic from pushing all lists
    to a single array to storing lists individually
    in localStorage.

--- src/storage.ts
+++ src/storage.ts
@@ -1,23 +1,21 @@
 import Task from "./task"
-import { createList } from "./list"
-
-export function storeList(name: string, index: number) {
-  const newList = createList(name)
-  localStorage.setItem(`${index}`, JSON.stringify(newList))
-}
 
 export function clearStorage() {
   localStorage.clear()
 }
 
-export function retrieveItem(index: number) {
-  const itemToParse = localStorage.getItem(`${index}`)
-  if (itemToParse) {
-    const parsed = JSON.parse(itemToParse)
-    parsed.tasks.forEach((task: Task) => {
-      Object.setPrototypeOf(task, Task.prototype)
-    })
-    return parsed
-  }
-  return
+export function storeList(name: string, list: { tasks: {}[] }) {
+  localStorage.setItem(name, JSON.stringify(list))
 }
+
+export function retrieveList(name: string) {
+  const toParse = localStorage.getItem(name)
+  if (toParse) {
+    const parsedList = JSON.parse(toParse)
+    parsedList.tasks.forEach((task: {}[]) => Object.setPrototypeOf(task, Task.prototype))
+    return parsedList
+  }
+  return false
+}
+
+document.querySelector(".resetdb")?.addEventListener("click", clearStorage)
