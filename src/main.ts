a8039b28cab9cf856ae26ac263c87c33e316633f
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-30 20:02:28 +0530

    refactor: Update imports, modify and/or add functions
    - Remove `listChanged` import and add `editListNameInLS`
    - Rename `screenController` to `domUtils` and import `delListFromDOM`
    - Modify `setCurrentList` in the mutation observer callback to accept
    an ID
    - Remove `dispatchEvent` from `addNewList`
    - Add `deleteList` function for list deletion

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import Task from "./task"
 import {
   clearStorage,
   deleteListFromLS,
+  editListNameInLS,
   getCurrentList,
-  listChanged,
   retrieveAllListsFromLS,
   retrieveListFromLS,
   setCurrentList,
   storeListInLS,
 } from "./localStorageUtils"
 import { createList, storeTaskInList } from "./list"
-import { renderExisting, renderListsToDOM } from "./screenController"
+import { delListFromDOM, reRenderList, renderExisting, renderListsToDOM } from "./domUtils"
 import setupListeners from "./listeners"
 import { addDays, format, isBefore } from "date-fns"
 
@@ -27,7 +27,7 @@ const taskContainer = document.querySelector("#tasks") as HTMLOListElement
 const listMutation = new MutationObserver(() => {
   const buttons = document.querySelectorAll("#lists button") as NodeListOf<HTMLButtonElement>
   if (buttons.length === 1) {
-    setCurrentList()
+    setCurrentList(buttons[0].dataset.id)
   }
   for (let button of buttons) {
     if (button) {
@@ -61,17 +61,31 @@ function switchList(event: MouseEvent) {
   // currentList = targetName
 }
 
-function addNewList(name: string) {
+export function addNewList(name: string) {
   if (!retrieveListFromLS(name)) {
     const newList = createList(name)
     storeListInLS(newList)
     renderListsToDOM(name, newList[0].id)
     setCurrentList(newList[0].id)
 
-    dispatchEvent(listChanged)
     renderExisting()
   }
 }
+
+export function deleteList() {
+  delListFromDOM()
+  deleteListFromLS()
+}
+
+// localStorage.removeItem(currentList)
+// localStorage.setItem(newName, JSON.stringify(listToChange))
+// currentList = listName.value
+// listTitle.innerText = listName.value
+
+// const listButton = document.querySelector(
+//   `#lists #list_${listToChange.id} button`,
+// ) as HTMLButtonElement
+// listButton.innerText = listName.value
 
 document.addEventListener(
   "DOMContentLoaded",
