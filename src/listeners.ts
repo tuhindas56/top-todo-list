a9af1d3371dee0285d52089b6dd95ba025d1b6eb
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-30 19:47:14 +0530

    refactor: Update event listeners, use functions from imported modules
    - Add imports for specific functions from "domUtils", "formHandling",
    and "main"
    - Update event listeners to use functions from the imported modules

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,28 +1,30 @@
-import * as domUtils from "./screenController"
+import { closeDialog, openDialog, openEditListDialog, deleteListBtnHandling } from "./domUtils"
+import { editListFormHandling, listFormHandling, listNameInputLengthCheck } from "./formHandling"
+import { deleteList } from "./main"
+
 const addListBtn = document.querySelector("#btn_add_list") as HTMLButtonElement
+const createListButton = document.querySelector("#btn_create_list") as HTMLButtonElement
+const listName = document.querySelector("#form_list_name") as HTMLInputElement
+const editListBtn = document.querySelector("#btn_edit_list_name") as HTMLButtonElement
+const changeListNameBtn = document.querySelector("#btn_change_list_name") as HTMLButtonElement
+const delListBtn = document.querySelector("#btn_del_list") as HTMLButtonElement
+const deletionModalAgreeBtn = document.querySelector(
+  "#deletion_modal_agree_btn",
+) as HTMLButtonElement
 
 export default () => {
-  addListBtn.addEventListener("click", () => domUtils.openDialog("list"))
-  // createListButton.addEventListener("click", listFormHandling)
-  // listName.addEventListener("input", listNameInputLengthCheck)
+  addListBtn.addEventListener("click", () => openDialog("list"))
+  createListButton.addEventListener("click", listFormHandling)
+  listName.addEventListener("input", listNameInputLengthCheck)
+  editListBtn.addEventListener("click", openEditListDialog)
+  changeListNameBtn.addEventListener("click", editListFormHandling)
+  delListBtn.addEventListener("click", deleteListBtnHandling)
 
-  // addTaskBtn.addEventListener("click", () => domUtils.openDialog("task"))
-  // createTaskButton.addEventListener("click", (event) => taskFormHandling(event, "task"))
+  deletionModalAgreeBtn.addEventListener("click", deleteList)
 
-  // delListBtn.addEventListener("click", deleteListBtnHandling)
-
-  // deletionModalAgreeBtn.addEventListener("click", () => {
-  //   deleteList()
-  // })
-
-  // const editListBtn = document.querySelector("#btn_edit_list_name") as HTMLButtonElement
-  // editListBtn.addEventListener("click", openEditListDialog)
-
-  // const changeListNameBtn = document.querySelector("#btn_change_list_name") as HTMLButtonElement
-  // changeListNameBtn.addEventListener("click", editListName)
   function closeBtnListeners(id: string) {
     const button = document.querySelector(`dialog#${id}_form button.btn_close`) as HTMLButtonElement
-    button.addEventListener("click", () => domUtils.closeDialog(id))
+    button.addEventListener("click", () => closeDialog(id))
   }
   closeBtnListeners("list")
   closeBtnListeners("task")
