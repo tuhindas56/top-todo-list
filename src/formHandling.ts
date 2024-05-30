e421dcce6d8c65c9d169276efea25334907bab96
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-30 19:45:04 +0530

    refactor Add input length check and edit form handling
    - Add `listNameInputLengthCheck` to validate list name input length
    - Add `editListFormHandling` to handle editing of list names
    - Refactor `listFormHandling` to use new imports and logic updates

--- src/formHandling.ts
+++ src/formHandling.ts
@@ -1,14 +1,40 @@
-function listFormHandling(event: MouseEvent) {
+import { addNewList } from "./main"
+import { openDialog, closeDialog, reRenderList } from "./domUtils"
+import { getCurrentList, editListNameInLS } from "./localStorageUtils"
+
+const listName = document.querySelector("#form_list_name") as HTMLInputElement
+const listError = document.querySelector("#list_error") as HTMLParagraphElement
+
+export function listFormHandling(event: MouseEvent) {
   event.preventDefault()
   if (["", null].includes(listName.value) || listName.value.length < 4) {
     listError.classList.remove("hidden")
     return
   }
-  addNewListItem(listName.value)
-  currentList = listName.value
+  addNewList(listName.value)
+  closeDialog("list")
+  openDialog("task", getCurrentList()![0].name)
+  const resetButton = document.querySelector('#list_form button[type="reset"]') as HTMLButtonElement
+  resetButton.click()
+}
 
-  domUtils.closeDialog("list")
-  domUtils.openDialog("task", currentList)
+export function listNameInputLengthCheck() {
+  if (listName.value.length >= 4) {
+    listError.classList.add("hidden")
+  } else {
+    listError.classList.remove("hidden")
+  }
+}
+
+export function editListFormHandling(event: MouseEvent) {
+  event.preventDefault()
+  if (["", null].includes(listName.value) || listName.value.length < 4) {
+    listError.classList.remove("hidden")
+    return
+  }
+  editListNameInLS(listName.value)
+  reRenderList()
+  closeDialog("list")
   const resetButton = document.querySelector('#list_form button[type="reset"]') as HTMLButtonElement
   resetButton.click()
 }
