eb014349ff7eac3b2f3da02a361d2fa31dc8a4c4
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-05-31 08:14:08 +0530

    refactor: Remove listNameInputCheck and related logic, add new listeners
     - Remove listNameInputCheck as an import.
     - Remove the variable caching the listName field from List creation
     form.
     - Add listeners for task creation form buttons

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,26 +1,28 @@
 import { closeDialog, openDialog, openEditListDialog, deleteListBtnHandling } from "./domUtils"
-import { editListFormHandling, listFormHandling, listNameInputLengthCheck } from "./formHandling"
+import { editListFormHandling, listFormHandling, taskFormHandling } from "./formHandling"
+import { getCurrentList } from "./localStorageUtils"
 import { deleteList } from "./main"
 
 const addListBtn = document.querySelector("#btn_add_list") as HTMLButtonElement
 const createListButton = document.querySelector("#btn_create_list") as HTMLButtonElement
-const listName = document.querySelector("#form_list_name") as HTMLInputElement
 const editListBtn = document.querySelector("#btn_edit_list_name") as HTMLButtonElement
 const changeListNameBtn = document.querySelector("#btn_change_list_name") as HTMLButtonElement
 const delListBtn = document.querySelector("#btn_del_list") as HTMLButtonElement
 const deletionModalAgreeBtn = document.querySelector(
   "#deletion_modal_agree_btn",
 ) as HTMLButtonElement
+const addTaskBtn = document.querySelector("#btn_add_task") as HTMLButtonElement
+const createTaskButton = document.querySelector("#task_btn") as HTMLButtonElement
 
 export default () => {
   addListBtn.addEventListener("click", () => openDialog("list"))
   createListButton.addEventListener("click", listFormHandling)
-  listName.addEventListener("input", listNameInputLengthCheck)
   editListBtn.addEventListener("click", openEditListDialog)
   changeListNameBtn.addEventListener("click", editListFormHandling)
   delListBtn.addEventListener("click", deleteListBtnHandling)
-
   deletionModalAgreeBtn.addEventListener("click", deleteList)
+  addTaskBtn.addEventListener("click", () => openDialog("task", getCurrentList()![0].name))
+  createTaskButton.addEventListener("click", (event) => taskFormHandling(event))
 
   function closeBtnListeners(id: string) {
     const button = document.querySelector(`dialog#${id}_form button.btn_close`) as HTMLButtonElement
