2d728e53b06ef8390c7c714ba63e89fd6de38ff5
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-01 18:17:27 +0530

    refactor: Add new list and tasks to be created on first page visit

--- src/init.ts
+++ src/init.ts
@@ -1,23 +1,36 @@
 import setupListeners from "./listeners"
-import { addDays, format } from "date-fns"
+import { addDays, format, sub } from "date-fns"
 import { addNewList, addNewTask } from "./main"
 import { renderExisting } from "./domRenderingUtils"
-import { retrieveAllTasksFromCurrentList, setCurrentList, clearStorage } from "./localStorageUtils"
+import { setCurrentList } from "./localStorageUtils"
 
 document.addEventListener(
   "DOMContentLoaded",
   () => {
     if (!JSON.parse(localStorage.getItem("visited")!)) {
-      addNewList("Demo")
-      addNewTask("second task", "fun", `${addDays(new Date(), 2)}`, 0)
-      addNewTask("test task", "testing fn", `${new Date()}`, 1)
+      addNewList("Sample")
+      addNewTask(
+        "Grocery shopping",
+        "Buy fruits, vegetables, milk, and bread for the week.",
+        `${sub(new Date(), { days: 2 })}`,
+        1,
+        true,
+      )
+      addNewTask("Pay bills", "Pay the electricity, internet, and water bills online.", `${new Date()}`, 0)
+      addNewTask("Call Mom", "Catch up with Mom over the phone this evening.", `${new Date()}`, 1)
+      addNewTask("Exercise", "Go for a 30-minute run in the park.", `${addDays(new Date(), 2)}`, 2)
+      addNewTask(
+        "Doctor's appointment",
+        "Visit Dr. John Doe for a routine check-up at 3 PM",
+        `${addDays(new Date(), 4)}`,
+        1,
+      )
     }
     const taskDue = document.querySelector(`#task_due`) as HTMLInputElement
     const editDue = document.querySelector("#edit_due") as HTMLInputElement
     taskDue.setAttribute("min", format(new Date(), "yyyy-MM-dd"))
     editDue.setAttribute("min", format(new Date(), "yyyy-MM-dd"))
 
-    console.table(retrieveAllTasksFromCurrentList())
     localStorage.setItem("visited", JSON.stringify(true))
     setCurrentList()
     setupListeners()
@@ -33,5 +46,3 @@ console.log(
       font-weight: 600;
       `,
 )
-
-document.querySelector(".resetdb")?.addEventListener("click", clearStorage)
